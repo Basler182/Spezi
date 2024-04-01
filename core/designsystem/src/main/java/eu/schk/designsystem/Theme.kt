d0cd0fa48e653a037f2c7a40373cfd64865f5fbb
Basler182 <basler1337@gmail.com>
2024-04-01 09:42:30 +0200

    update Color Theme

--- core/designsystem/src/main/java/eu/schk/designsystem/Theme.kt
+++ core/designsystem/src/main/java/eu/schk/designsystem/Theme.kt
@@ -16,32 +16,30 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = CardinalRed,
+    secondary = CoolGrey,
+    tertiary = CardinalRedLight
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = CardinalRed,
+    secondary = CoolGrey,
+    tertiary = RectangleBlue,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = White,
+    surface = White,
+    onPrimary = White,
+    onSecondary = White,
+    onTertiary = White,
+    onBackground = Black80,
+    onSurface = Black80,
 )
 
 @Composable
 fun SpeziTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
