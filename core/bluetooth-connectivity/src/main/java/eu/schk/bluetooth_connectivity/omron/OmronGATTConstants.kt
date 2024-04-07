f14928f837a3bc1df9b3e1e9272dcecbc49d76ba
Kilian Schneider <basler1337@gmail.com>
2024-04-07 19:32:41 +0200

    feat omron gatt constants

---
+++ core/bluetooth-connectivity/src/main/java/eu/schk/bluetooth_connectivity/omron/OmronGATTConstants.kt
@@ -0,0 +1,68 @@
+package eu.schk.bluetooth_connectivity.omron
+
+import java.util.UUID
+
+
+val omronServiceMap = OmronService.entries.associateBy { it.uuid }
+
+fun isServiceUUIDInOmronServices(serviceUUID: UUID): Boolean {
+    return omronServiceMap.containsKey(serviceUUID)
+}
+
+enum class OmronDescriptors(val uuid: UUID, val descriptorName: String) {
+    MODEL_NUMBER_CHARACTERISTICS(
+        UUID.fromString("00002a24-0000-1000-8000-00805f9b34fb"),
+        "Model Number String"
+    ),
+
+    BATTERY_LEVEL(
+        UUID.fromString("00002a19-0000-1000-8000-00805f9b34fb"),
+        "Battery Level"
+    ),
+
+    CURRENT_TIME(
+        UUID.fromString("00002a2b-0000-1000-8000-00805f9b34fb"),
+        "Current Time"
+    ),
+
+    BLOOD_PRESSURE_FEATURE(
+        UUID.fromString("00002a49-0000-1000-8000-00805f9b34fb"),
+        "Blood Pressure Feature"
+    ),
+    BLOOD_PRESSURE_MEASUREMENT(
+        UUID.fromString("00002a35-0000-1000-8000-00805f9b34fb"),
+        "Blood Pressure Measurement"
+    ),
+}
+
+
+enum class OmronService(
+    val uuid: UUID,
+    val serviceName: String,
+    val descriptors: List<OmronDescriptors>
+) {
+    DEVICE_INFORMATION(
+        UUID.fromString("0000180a-0000-1000-8000-00805f9b34fb"),
+        "Device Information Service",
+        listOf(OmronDescriptors.MODEL_NUMBER_CHARACTERISTICS)
+    ),
+    BATTERY(
+        UUID.fromString("0000180f-0000-1000-8000-00805f9b34fb"),
+        "Battery Service",
+        listOf(OmronDescriptors.BATTERY_LEVEL)
+    ),
+    CURRENT_TIME(
+        UUID.fromString("00001805-0000-1000-8000-00805f9b34fb"),
+        "Current Time Service",
+        listOf(OmronDescriptors.CURRENT_TIME)
+    ),
+
+    BLOOD_PRESSURE(
+        UUID.fromString("00001810-0000-1000-8000-00805f9b34fb"),
+        "Blood Pressure Service",
+        listOf(
+            OmronDescriptors.BLOOD_PRESSURE_FEATURE, // 0500
+            OmronDescriptors.BLOOD_PRESSURE_MEASUREMENT
+        )
+    )
+}
